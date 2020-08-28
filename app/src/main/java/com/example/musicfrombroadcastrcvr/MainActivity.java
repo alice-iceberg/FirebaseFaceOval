f0a5a24c55ad16315a65f5ce132e6d77dd8feb22
alice-iceberg <aliceblackwood123@gmail.com>
2020-08-28 14:42:21 +0900

    cropping only face

--- app/src/main/java/com/example/musicfrombroadcastrcvr/MainActivity.java
+++ app/src/main/java/com/example/musicfrombroadcastrcvr/MainActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -47,18 +49,18 @@ public class MainActivity extends AppCompatActivity {
         final Bitmap bitmap_photo = BitmapFactory.decodeResource(this.getResources(), R.drawable.woman);
         final InputImage image = InputImage.fromBitmap(bitmap_photo, 0); //check second parameter
 
-        FaceDetector detector = getClient(options);
+        final FaceDetector detector = getClient(options);
         detector.process(image).addOnSuccessListener(new OnSuccessListener<List<Face>>() {
             @RequiresApi(api = Build.VERSION_CODES.P)
             @Override
             public void onSuccess(List<Face> faces) {
-                Log.e("TAG", "onSuccess: Face detected" );
+                Log.e("TAG", "onSuccess: Face detected");
                 FaceContour contour = faces.get(0).getContour(FaceContour.FACE);
 
                 Bitmap mutableBitmap = image.getBitmapInternal().copy(Bitmap.Config.ARGB_8888, true);
                 Canvas canvas = new Canvas(mutableBitmap);
                 Paint myPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-                myPaint.setColor(Color.BLUE);
+                myPaint.setColor(Color.parseColor("#99ff0000"));
                 Path path = new Path();
                 assert contour != null;
                 path.moveTo(contour.getPoints().get(0).x, contour.getPoints().get(0).y);
@@ -66,8 +68,24 @@ public class MainActivity extends AppCompatActivity {
                     path.lineTo(item.x, item.y);
                 }
                 path.close();
-                canvas.drawPath(path, myPaint);
-                photo.setImageBitmap(mutableBitmap);
+                detector.close();
+
+                //canvas.drawPath(path, myPaint);
+                //photo.setImageBitmap(mutableBitmap);
+
+
+                //my trial
+                Bitmap output_my = Bitmap.createBitmap(mutableBitmap.getWidth(), mutableBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas_mine = new Canvas(output_my);
+                Paint paint_mine = new Paint(Paint.ANTI_ALIAS_FLAG);
+                paint_mine.setColor(Color.BLUE);
+                canvas_mine.drawPath(path, paint_mine);
+                paint_mine.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+                canvas_mine.drawBitmap(bitmap_photo, 0, 0, paint_mine);
+                photo.setImageBitmap(output_my);
+
+
+
             }
 
         })
